feb29f2dfa2ab010f01bb014556b35c4396bb074
Melza-Geno-I <melzageno.i@gmail.com>
2023-11-29 12:29:44 +0530

    Added sort function

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,10 @@ let tasks = [];
 const input = document.getElementById('input');
 const addBtn = document.getElementById('addBtn');
 
-//Add value from the input field (start) 
+//Add function: To add value from the input field (start) 
 addBtn.addEventListener('click',()=>{
     let inputValue = input.value;
-    if(!input.value){
+    if(!inputValue){
         alert("No Task entered to add!");
         return;
     }
@@ -17,62 +17,66 @@ addBtn.addEventListener('click',()=>{
         status: 'Pending'
     });
     input.value='';
-    displayAllTasks();
+    sortTasks();
 })
 //Add value from the input field (end)
 
-const taskListContainer = document.getElementById('all_tasks');
-
-//Display fucntion to display the rendered task in the Todo list (start)
-function displayAllTasks(){
-    htmlString = '';
-    
-    tasks.forEach(element => {
-        htmlString += `
-            <div id="${element.id}" class="row">
-                <div class="column1">${element.task}</div>
-                <div class="column2"><button class="status_button ${element.status}">${element.status}</button></div>
-                <div class="column3"><i class="fa-solid fa-trash-can"></i></div>
-            </div>
-        `
-    });
-    
-taskListContainer.innerHTML = htmlString;
-   
+//Display function: To display the rendered task in the Todo list (start)
+function taskHtml(data){    
+    return `
+                <div id="${data.id}" class="row">
+                    <div class="column1">${data.task}</div>
+                    <div class="column2"><button class="status_button ${data.status}">${data.status}</button></div>
+                    <div class="column3"><i class="fa-solid fa-trash-can"></i></div>
+                </div>
+            `   
 }
 //display task in the Todo list (end)
 
-
-//Adding event listeners for each status button (start)
-taskListContainer.addEventListener('click', function (event) {
-if (event.target.classList.contains('status_button')) {
-    const taskId = event.target.parentNode.parentNode.id;
-console.log(taskId);
-const task = tasks.find( element => element.id == taskId )
-if(task){
-    toggle(event);
-}
-    
-}
-});
-//Adding event listeners for each status button (end)
-
-//Toggle function to toggle between completed and pending (start)
+//Toggle function: To toggle between completed and pending (start)
 function toggle(event){
-        if(event.target.classList.contains('completed'))
-        {
-            event.target.classList.remove('completed');
-            event.target.classList.add('Pending');
-            event.target.innerHTML="Pending";
-        } else if(event.target.classList.contains('Pending')){
-            event.target.classList.remove('Pending');
-            event.target.classList.add('completed');
-            event.target.innerHTML="Completed";
+    const taskId = event.target.parentNode.parentNode.id;
+    const task = tasks.find((element) => element.id == taskId);
+    if(task){
+        if(task.status === 'Completed'){
+            task.status = "Pending";
+        }else{
+            task.status = "Completed";
         }
+    }else{
+        console.log("Task not found for ID",taskId);
     }
+    sortTasks();
+}
 //Toggle function to toggle between completed and pending (end)
 
+// Sort Function: To sort based on task status (start)
+function sortTasks(){
+    let htmlString = '';
+    const taskListContainer = document.getElementById('all_tasks');
 
+    const pendingTasks = tasks.filter((element) => element.status !== 'Completed');
+    const completedTasks = tasks.filter((element) => element.status === 'Completed');
+
+    pendingTasks.forEach((task)=>{
+        htmlString += taskHtml(task);
+    });
+
+    completedTasks.forEach((task)=>{
+        htmlString += taskHtml(task);
+    });
+
+    taskListContainer.innerHTML = htmlString;
+
+    //Adding event listeners for each status button (start)
+    const statusButtons = document.querySelectorAll('.status_button');
+    statusButtons.forEach((button) => button.addEventListener('click', toggle) );
+    //Adding event listeners for each status button (end)
+}
+// Sort Function (end)
+
+//initial data load
+sortTasks();
 
 
 
